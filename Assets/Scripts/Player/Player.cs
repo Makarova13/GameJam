389db371239af67de4d7093ab1ee7b72aa0e86b3
Cloud-NL <ethancarver2004@gmail.com>
2024-04-14 14:45:08 +0200

    Player Movement Added

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private InputActionReference movement;
+    [SerializeField] private Rigidbody playerRB;
+    [Space]
+    [Header("Vectors")]
+    private Vector3 movementInput;
+    [Space]
+    [Header("Floats")]
+    private float PlayerSpeed = 10f;
+
+    void Update()
+    {
+        movementInput = movement.action.ReadValue<Vector3>();
+        playerRB.AddForce(movementInput * PlayerSpeed * Time.deltaTime, ForceMode.Impulse);
+    }
+}
