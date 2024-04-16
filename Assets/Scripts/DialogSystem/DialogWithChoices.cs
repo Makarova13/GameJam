203c264bdf2f57a1f5e8d9e2170311745ef9e0dd
Cloud-NL <ethancarver2004@gmail.com>
2024-04-16 19:14:10 +0200

    PlaceHolders Added / Movement Direction

---
+++ Assets/Scripts/DialogSystem/DialogWithChoices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+    [Serializable]
+    public class DialogsWithChoices
+    {
+        public List<DialogWithChoices> Dialogs;
+    }
+
+    [Serializable]
+    public class DialogWithChoices
+    {
+        public string Text;
+        public List<Choice> Choices;
+    }
+
+    [Serializable]
+    public class Choice
+    {
+        public string Text;
+        public ChoiceType ChoiceType;
+        public string Response;
+    }
+
+    public enum ChoiceType
+    {
+        Neutrual = -1,
+        Wrong = 0,
+        Right = 1,
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,17 +7,52 @@ public class Player : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] private InputActionReference movement;
+    [SerializeField] private Animator animator;
     [SerializeField] private Rigidbody playerRB;
     [Space]
     [Header("Vectors")]
     private Vector3 movementInput;
     [Space]
     [Header("Floats")]
-    private float PlayerSpeed = 10f;
+    private float PlayerSpeed;
+    private float CurrentSpeed;
+    private float Direction = 0;
+
+    private void Start()
+    {
+        Speed = 60f;
+    }
 
     void FixedUpdate()
     {
         movementInput = movement.action.ReadValue<Vector3>();
-        playerRB.AddForce(movementInput * PlayerSpeed * Time.fixedDeltaTime, ForceMode.Impulse);
+        CurrentSpeed = movementInput.magnitude * PlayerSpeed;
+        playerRB.AddForce(movementInput * Speed * Time.fixedDeltaTime, ForceMode.Impulse);
+        animator.SetFloat("Walking", CurrentSpeed);
+        if (movementInput.x < 0)
+        {
+            Debug.Log("Links");
+        } else if(movementInput.x > 0)
+        {
+            Debug.Log("Rechts");
+        } else if(movementInput.z < 0)
+        {
+            Debug.Log("Naar Beneden");
+        } else if(movementInput.z > 0)
+        {
+            Debug.Log("Naar Boven");
+        }
+    }
+
+    public float Speed
+    {
+        get { return PlayerSpeed;  }
+        set
+        {
+            if(value < 100)
+            {
+                PlayerSpeed = value;
+            }
+        }
     }
 }
