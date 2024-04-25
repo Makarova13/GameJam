60a721c5f13b9ce7fee049b6b1c10e00180f6e02
Cloud-NL <ethancarver2004@gmail.com>
2024-04-25 19:07:49 +0200

    StartGame UI Implementation

--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -7,19 +7,46 @@ using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
-    [SerializeField] private Button startGameButton;
+    [SerializeField] private Button startGameButton, howToBtn, settingsBtn, creditsBtn, closeBtn_howTo, closeBtn_settings, closeBtn_credits;
     [SerializeField] private int gameSceneIndex = 1;
-    [SerializeField] private Button quitGameButton;
+    [SerializeField] private GameObject howToPlay, mainScene, settings, credits;
 
     private void Awake()
     {
         startGameButton.onClick.AddListener(OnStartGameClicked);
-        quitGameButton.onClick.AddListener(OnQuitGameClicked);
+        howToBtn.onClick.AddListener(OnHowTo);
+        settingsBtn.onClick.AddListener(OnSettings);
+        creditsBtn.onClick.AddListener(OnCredits);
     }
 
-    private void OnQuitGameClicked()
+    private void OnHowTo()
     {
-        Application.Quit();
+        mainScene.SetActive(false);
+        howToPlay.SetActive(true);
+
+        closeBtn_howTo.onClick.AddListener(Close);
+    }
+
+    private void OnSettings()
+    {
+        mainScene.SetActive(false);
+        settings.SetActive(true);
+        closeBtn_settings.onClick.AddListener(Close);
+    }
+
+    private void OnCredits()
+    {
+        mainScene.SetActive(false);
+        credits.SetActive(true);
+        closeBtn_credits.onClick.AddListener(Close);
+    }
+
+    private void Close()
+    {
+        if (howToPlay.activeSelf) howToPlay.SetActive(false);
+        if (settings.activeSelf) settings.SetActive(false);
+        if (credits.activeSelf) credits.SetActive(false);
+        mainScene.SetActive(true);
     }
 
     private void OnStartGameClicked()
